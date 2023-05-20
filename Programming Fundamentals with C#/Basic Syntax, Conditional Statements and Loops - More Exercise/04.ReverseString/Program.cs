4ac22fc757f66639ce9f8c09d1fbb140d2605595
SPalahanov <slavcho.palahanov@gmail.com>
2023-05-20 16:42:59 +0300

    Basic Syntax, Conditional Statements and Loops - More Exercise

---
+++ Programming Fundamentals with C#/Basic Syntax, Conditional Statements and Loops - More Exercise/04.ReverseString/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04.ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string newText = "";
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                newText += text[i];
+            }
+
+            Console.WriteLine(newText);
+        }
+    }
+}
