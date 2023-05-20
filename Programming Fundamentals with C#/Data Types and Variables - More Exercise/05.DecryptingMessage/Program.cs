91a7184048133ea881c35678eed088c1c6e34e38
SPalahanov <slavcho.palahanov@gmail.com>
2023-05-20 16:36:24 +0300

    Upload Data Types and Variables - More Exercise

---
+++ Programming Fundamentals with C#/Data Types and Variables - More Exercise/05.DecryptingMessage/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _05.DecryptingMessage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            string word = "";
+
+            for (int i = 1; i <= numberOfLines; i++)
+            {
+                char symbol = char.Parse(Console.ReadLine());
+                int numberInDecimal = (int)symbol;
+                char newsymbol = (char)(numberInDecimal + key);
+                string letter = newsymbol.ToString();
+                word += letter;
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
