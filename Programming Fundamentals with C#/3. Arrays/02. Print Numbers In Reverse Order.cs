7212c9195eda222bedc7d17ff0936b122076e7f8
SPalahanov <slavcho.palahanov@gmail.com>
2023-06-07 16:29:14 +0300

    Lab - Arrays

---
+++ Programming Fundamentals with C#/3. Arrays/02. Print Numbers In Reverse Order.cs
@@ -0,0 +1,21 @@
+﻿namespace _02._Print_Numbers_In_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrayLength = int.Parse(Console.ReadLine());
+            int[] numbers = new int[arrayLength];
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = arrayLength - 1; i >= 0; i--)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/3. Arrays/04. Reverse Array Of Strings.cs
@@ -0,0 +1,17 @@
+﻿namespace _04._Reverse_Array_Of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{input[i]} ");
+            }
+        }
+    }
+}
