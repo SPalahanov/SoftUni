582b72e92205f21fc62efd64e42c85265d87c173
SPalahanov <slavcho.palahanov@gmail.com>
2023-06-07 16:26:34 +0300

    Lab  - Data Types and Variables

---
+++ Programming Fundamentals with C#/2. Data Types and Variables/12. Refactor Special Numbers.cs
@@ -0,0 +1,26 @@
+﻿namespace _12.RefactorSpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfNumbers = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int num = 1; num <= countOfNumbers; num++)
+            {
+                int digit = num;
+                int sumOfDigit = 0;
+                while (digit != 0)
+                {
+                    sumOfDigit += digit % 10;
+                    digit = digit / 10;
+                }
+                bool isSpecialNum = (sumOfDigit == 5) || (sumOfDigit == 7) || (sumOfDigit == 11);
+
+                Console.WriteLine($"{num} -> {isSpecialNum}");
+            }
+
+        }
+    }
+}
