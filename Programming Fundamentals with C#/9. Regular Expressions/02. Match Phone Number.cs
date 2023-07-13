9fae7dcbf89a85b52ec752c9739f98938bd259f8
SPalahanov <slavcho.palahanov@gmail.com>
2023-07-13 18:22:56 +0300

    Regular Expressions

---
+++ Programming Fundamentals with C#/9. Regular Expressions/02. Match Phone Number.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern =
+                @"([+][3][5][9])(?:\-)([2])(?:\-)(\d{3}(?:\-)\d{4})|([+][3][5][9])(?:\ )([2])(?:\ )(\d{3}(?:\ )\d{4})";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            var machedPhones = matches
+                .Cast<Match>()
+                .Select(x => x.Value.Trim())
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", machedPhones));
+        }
+    }
+}
