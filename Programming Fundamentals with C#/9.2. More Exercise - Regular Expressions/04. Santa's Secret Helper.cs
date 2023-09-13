2476733ba3ea1c7420309330785759064a5570c1
SPalahanov <slavcho.palahanov@gmail.com>
2023-09-13 11:32:35 +0300

    More Exercise - Regular Expressions

---
+++ Programming Fundamentals with C#/9.2. More Exercise - Regular Expressions/04. Santa's Secret Helper.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _04._Santa_s_Secret_Helper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+
+            string input;
+
+            List<string> goodChildren = new List<string>();
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var letter in input)
+                {
+                    int letterNum = letter;
+                    int newLetterNumber = letterNum - key;
+
+                    char newLetter = (char)newLetterNumber;
+
+                    sb.Append(newLetter);
+                }
+
+                string pattern = @"@([A-Za-z]+)[^@\-!:>]*!([GN])!";
+
+                Regex regex = new Regex(pattern);
+
+                MatchCollection matches = regex.Matches(sb.ToString());
+
+                foreach (Match match in matches)
+                {
+                    if (match.Groups[2].Value == "G")
+                    {
+                        goodChildren.Add(match.Groups[1].Value);
+                    }
+                }
+            }
+
+            foreach (var child in goodChildren)
+            {
+                Console.WriteLine(child);
+            }
+        }
+    }
+}
