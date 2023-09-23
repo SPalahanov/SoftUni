b25a5bea88721597e8b8f3a40035d2d6980ab0d1
SPalahanov <slavcho.palahanov@gmail.com>
2023-09-23 15:29:39 +0300

    Create  01. Unique Usernames

---
+++ C# Advanced/3.1. Exercise - Sets and Dictionaries Advanced/01. Unique Usernames.cs
@@ -0,0 +1,24 @@
+﻿namespace _01._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> userNames = new();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+
+                userNames.Add(name);
+            }
+
+            foreach (var userName in userNames)
+            {
+                Console.WriteLine(userName);
+            }
+        }
+    }
+}
