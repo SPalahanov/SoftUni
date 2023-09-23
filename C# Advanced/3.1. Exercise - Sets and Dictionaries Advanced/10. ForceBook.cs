116ce80a679515a28295943d40352d1b64330b15
SPalahanov <slavcho.palahanov@gmail.com>
2023-09-23 15:49:49 +0300

    Update file 10. ForceBook

--- C# Advanced/3.1. Exercise - Sets and Dictionaries Advanced/10. ForceBook.cs
+++ C# Advanced/3.1. Exercise - Sets and Dictionaries Advanced/10. ForceBook.cs
@@ -4,7 +4,66 @@
     {
         static void Main(string[] args)
         {
-            
+            SortedDictionary<string, SortedSet<string>> sidesUsers = new SortedDictionary<string, SortedSet<string>>();
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "Lumpawaroo")
+            {
+                if (input.Contains(" | "))
+                {
+                    string[] tokens = input.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+                    string side = tokens[0];
+                    string user = tokens[1];
+
+                    if (!sidesUsers.Values.Any(u => u.Contains(user)))
+                    {
+                        if (!sidesUsers.ContainsKey(side))
+                        {
+                            sidesUsers.Add(side, new SortedSet<string>());
+                        }
+
+                        sidesUsers[side].Add(user);
+                    }
+                }
+                else
+                {
+                    string[] tokens = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                    string side = tokens[1];
+                    string user = tokens[0];
+
+                    foreach (var sideUsers in sidesUsers)
+                    {
+                        if (sideUsers.Value.Contains(user))
+                        {
+                            sideUsers.Value.Remove(user);
+                            break;
+                        }
+                    }
+
+                    if (!sidesUsers.ContainsKey(side))
+                    {
+                        sidesUsers.Add(side, new SortedSet<string>());
+                    }
+
+                    sidesUsers[side].Add(user);
+
+                    Console.WriteLine($"{user} joins the {side} side!");
+                }
+            }
+
+            foreach (var sideUsers in sidesUsers.OrderByDescending(su => su.Value.Count))
+            {
+                if (sideUsers.Value.Any())
+                {
+                    Console.WriteLine($"Side: {sideUsers.Key}, Members: {sideUsers.Value.Count}");
+
+                    foreach (var user in sideUsers.Value)
+                    {
+                        Console.WriteLine($"! {user}");
+                    }
+                }
+            }
         }
     }
 }
