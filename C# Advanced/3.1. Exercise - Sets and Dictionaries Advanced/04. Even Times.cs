3bebe93b36786bc2a8ad958e2b8145cd95e1348e
SPalahanov <slavcho.palahanov@gmail.com>
2023-09-23 15:35:26 +0300

    Create 04. Even Times

---
+++ C# Advanced/3.1. Exercise - Sets and Dictionaries Advanced/04. Even Times.cs
@@ -0,0 +1,28 @@
+﻿namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> numbersCounts = new Dictionary<int, int>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (!numbersCounts.ContainsKey(number))
+                {
+                    numbersCounts.Add(number, 0);
+                }
+
+                numbersCounts[number]++;
+            }
+
+            int result = numbersCounts.Single(x => x.Value % 2 == 0).Key;
+
+            Console.WriteLine(result);
+        }
+    }
+}
