cdadaf00f14c10205a73591e39d38576b4d06e1f
SPalahanov <slavcho.palahanov@gmail.com>
2023-09-23 15:47:26 +0300

    Update file 09. SoftUni Exam Results

--- C# Advanced/3.1. Exercise - Sets and Dictionaries Advanced/09. SoftUni Exam Results.cs
+++ C# Advanced/3.1. Exercise - Sets and Dictionaries Advanced/09. SoftUni Exam Results.cs
@@ -6,7 +6,58 @@ namespace _09._SoftUni_Exam_Results
     {
         static void Main(string[] args)
         {
-            
+            SortedDictionary<string, int> participantsPoints = new();
+            SortedDictionary<string, int> languagesSubmissions = new();
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "exam finished")
+            {
+                string[] arguments = input.Split("-");
+
+
+                string userName = arguments[0];
+
+                if (arguments[1] == "banned")
+                {
+                    participantsPoints.Remove(userName);
+                    continue;
+                }
+
+                string language = arguments[1];
+                int points = int.Parse(arguments[2]);
+
+                if (!participantsPoints.ContainsKey(userName))
+                {
+                    participantsPoints.Add(userName, points);
+                }
+
+                if (participantsPoints[userName] < points)
+                {
+                    participantsPoints[userName] = points;
+                }
+
+                if (!languagesSubmissions.ContainsKey(language))
+                {
+                    languagesSubmissions.Add(language, 0);
+                }
+
+                languagesSubmissions[language]++;
+            }
+
+            Console.WriteLine($"Results:");
+
+            foreach (var participantPoint in participantsPoints.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"{participantPoint.Key} | {participantPoint.Value}");
+            }
+
+            Console.WriteLine($"Submissions:");
+
+            foreach (var languageSubmission in languagesSubmissions.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"{languageSubmission.Key} - {languageSubmission.Value}");
+            }
         }
     }
 }
