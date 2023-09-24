5627e8ba651310364ab327e933968c2c9d0537d9
SPalahanov <slavcho.palahanov@gmail.com>
2023-09-24 18:27:17 +0300

    Create new project: 07.Folder Size

---
+++ C# Advanced/Streams, Files and Directories/07. Folder Size/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07._Folder_Size
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
