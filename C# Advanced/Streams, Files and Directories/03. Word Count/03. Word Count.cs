a7b3a56a10f87a3f9868705d8d6ba64d5bfb931e
SPalahanov <slavcho.palahanov@gmail.com>
2023-09-24 16:43:22 +0300

    CalculateWordsCounts Method

--- C# Advanced/Streams, Files and Directories/03. Word Count/03. Word Count.cs
+++ C# Advanced/Streams, Files and Directories/03. Word Count/03. Word Count.cs
@@ -17,6 +17,42 @@
 
         public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
         {
+            Dictionary<string, int> wordAndFrequency = new Dictionary<string, int>();
+            string[] words = File.ReadAllText(wordsFilePath).Split();
+
+            using (StreamReader reader = new StreamReader(textFilePath))
+            {
+                string currentLine = reader.ReadLine();
+
+                while (currentLine != null)
+                {
+                    string[] wordsInCurrentLine = currentLine.ToLower()
+                        .Split(new[] { ' ', '.', ',', '-', '?', '!', ':', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var word in words)
+                    {
+                        foreach (var item in wordsInCurrentLine)
+                        {
+                            if (word == item)
+                            {
+                                if (wordAndFrequency.ContainsKey(item) == false)
+                                {
+                                    wordAndFrequency.Add(item, 0);
+                                }
+                                wordAndFrequency[item]++;
+                            }
+                        }
+                    }
+                    currentLine = reader.ReadLine();
+                }
+            }
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                foreach (var item in wordAndFrequency.OrderByDescending(x => x.Value))
+                {
+                    writer.WriteLine($"{item.Key} - {item.Value}");
+                }
+            }
 
         }
     }
