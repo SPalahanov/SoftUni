212c9a3d7b5be51d2955237a39d05a8b7fcbf130
SPalahanov <slavcho.palahanov@gmail.com>
2023-09-24 09:32:18 +0300

    Update 01. Odd Lines

--- C# Advanced/Streams, Files and Directories/01. Odd Lines/01. Odd Lines.cs
+++ C# Advanced/Streams, Files and Directories/01. Odd Lines/01. Odd Lines.cs
@@ -1,10 +1,36 @@
-﻿namespace _01._Odd_Lines
+﻿namespace OddLines
 {
-    internal class Program
+    using System.IO;
+	
+    public class OddLines
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    int lineNumber = 0;
+
+                    while (reader.EndOfStream == false)
+                    {
+                        string line = reader.ReadLine();
+
+                        if (lineNumber++ % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                    }
+                }
+            }
         }
     }
 }
