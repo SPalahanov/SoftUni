725d8c42c58e88fc82801496a9625928497e554a
SPalahanov <slavcho.palahanov@gmail.com>
2023-09-24 17:08:32 +0300

    MergeTextFiles Method

--- C# Advanced/Streams, Files and Directories/04. Merge Text Files/04. Merge Text Files.cs
+++ C# Advanced/Streams, Files and Directories/04. Merge Text Files/04. Merge Text Files.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.IO;
+    using System.Reflection.PortableExecutable;
+
     public class MergeFiles
     {
         static void Main()
@@ -15,6 +17,33 @@
 
         public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
         {
+            List<string> inputs = new List<string>();
+
+            using (StreamReader firstReader = new StreamReader(firstInputFilePath))
+            using (StreamReader secondReader = new StreamReader(secondInputFilePath))
+            {
+                while (firstReader.EndOfStream == false)
+                {
+                    string firstInput = firstReader.ReadLine();
+                    inputs.Add(firstInput);
+                }
+
+                while (secondReader.EndOfStream == false)
+                {
+                    string secondInput = secondReader.ReadLine();
+                    inputs.Add(secondInput);
+                }
+                
+                inputs.Sort();
+            }
+
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                foreach (var input in inputs)
+                {
+                    writer.WriteLine(input);
+                }
+            }
         }
     }
 }
