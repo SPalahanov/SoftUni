eef3100a7e6536fd733d052249264c2c10213e13
SPalahanov <slavcho.palahanov@gmail.com>
2023-09-24 18:45:39 +0300

    07. Folder Size

--- C# Advanced/Streams, Files and Directories/07. Folder Size/07. Folder Size.cs
+++ C# Advanced/Streams, Files and Directories/07. Folder Size/07. Folder Size.cs
@@ -1,4 +1,6 @@
-﻿namespace FolderSize
+﻿using System.Net.Security;
+
+namespace FolderSize
 {
     using System;
     using System.IO;
@@ -14,6 +16,35 @@
 
         public static void GetFolderSize(string folderPath, string outputFilePath)
         {
+            long size = CalculateFolderSize(folderPath, 0);
+
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                writer.WriteLine($"{size / 1024.0} KB");
+            }
+        }
+
+        public static long CalculateFolderSize(string folderPath,  int level)
+        {
+            string[] files = Directory.GetFiles(folderPath);
+
+            long bytes = 0;
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo info = new FileInfo(files[i]);
+
+                bytes += info.Length;
+            }
+
+            string[] directories = Directory.GetDirectories(folderPath);
+
+            foreach (var directoryPath in directories)
+            {
+                bytes += CalculateFolderSize(directoryPath, level + 1);
+            }
+
+            return bytes;
         }
     }
 }
