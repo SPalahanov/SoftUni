52429609dfbb5bc9989b022267ddf66e9f409b21
SPalahanov <slavcho.palahanov@gmail.com>
2023-09-30 00:34:07 +0300

    03. Count Uppercase Words

---
+++ C# Advanced/5. Functional Programming/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Reflection.Metadata;
+using System.Xml.XPath;
+
+namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> checker = n => n[0] == n.ToUpper()[0];
+
+            string[] words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => checker(x))
+                .ToArray();
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
