30660eb077ada8fb817033a836017300be1312bd
SPalahanov <slavcho.palahanov@gmail.com>
2023-09-30 00:34:35 +0300

    04. Add VAT

---
+++ C# Advanced/5. Functional Programming/04. Add VAT/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+namespace _04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, double> parser = s => double.Parse(s);
+            Func<double, double> vat = n =>  n * 1.2;
+
+            List<double> numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .Select(vat)
+            .ToList();
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{(number):f2}");
+            }
+        }
+    }
+}
