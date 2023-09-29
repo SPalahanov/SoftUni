75817b0296a05786111098fadca3b567e91d8dbb
SPalahanov <slavcho.palahanov@gmail.com>
2023-09-30 00:33:32 +0300

    Upload 02. Sum Numbers

---
+++ C# Advanced/5. Functional Programming/02. Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Immutable;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _02._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+
+            Console.WriteLine(numbers.Count);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
