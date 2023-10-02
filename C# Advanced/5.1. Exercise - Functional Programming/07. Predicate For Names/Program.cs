ca023d6fcdc7a23f728da0717bf9888d064ab348
SPalahanov <slavcho.palahanov@gmail.com>
2023-10-02 06:28:15 +0300

    Upload new project: 07. Predicate For Names

---
+++ C# Advanced/5.1. Exercise - Functional Programming/07. Predicate For Names/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<List<string>, Predicate<string>, List<string>> predicate = (names, match) =>
+            {
+                List<string> result = new();
+
+                foreach (var name in names)
+                {
+                    if (match(name))
+                    {
+                        result.Add(name);
+                    }
+                }
+
+                return result;
+            };
+
+            Action<List<string>> print = finnalList =>
+                Console.WriteLine(string.Join(Environment.NewLine, finnalList));
+
+                int lenght = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Predicate<string> checkLength = name => name.Length <= lenght;
+
+            names = predicate(names, checkLength);
+
+            print(names);
+        }
+    }
+}
