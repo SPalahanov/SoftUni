0474c01520e53271b65d30f7cfd35fbc4d8e1361
SPalahanov <slavcho.palahanov@gmail.com>
2023-10-02 06:23:10 +0300

    Upload new project: 01. Action Print

---
+++ C# Advanced/5.1. Exercise - Functional Programming/01. Action Print/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Channels;
+
+namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> print = strings => 
+                Console.WriteLine(string.Join(Environment.NewLine, strings));
+
+            string[] strings = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            print(strings);
+        }
+    }
+}
