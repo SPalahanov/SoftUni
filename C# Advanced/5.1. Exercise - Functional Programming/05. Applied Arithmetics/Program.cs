923f1f41ac813080d14748d3b39a8fdef69278a4
SPalahanov <slavcho.palahanov@gmail.com>
2023-10-02 06:26:33 +0300

    Upload new project: 05. Applied Arithmetics

---
+++ C# Advanced/5.1. Exercise - Functional Programming/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Globalization;
+
+namespace _05._Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, List<int>, List<int>> calculate = (command, numbers) =>
+            {
+                List<int> result = new();
+
+                foreach (int number in numbers)
+                {
+                    if (command == "add")
+                    {
+                        result.Add(number + 1);
+                    }
+
+                    if (command == "multiply")
+                    {
+                        result.Add(number * 2);
+                    }
+
+                    if (command == "subtract")
+                    {
+                        result.Add(number - 1);
+                    }
+                }
+
+                return result;
+            };
+
+            Action<List<int>> print = numbers =>
+                Console.WriteLine(string.Join(" ", numbers));
+
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "print")
+                {
+                    print(numbers);
+                }
+                else
+                {
+                    numbers = calculate(command, numbers);
+                }
+
+            }
+        }
+    }
+}
