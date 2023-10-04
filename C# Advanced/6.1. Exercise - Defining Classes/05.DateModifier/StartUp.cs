5266ad6219d362659f66dcfe0fe0bb774405f811
SPalahanov <slavcho.palahanov@gmail.com>
2023-10-04 23:49:39 +0300

    Upload 05.DateModifier

---
+++ C# Advanced/6.1. Exercise - Defining Classes/05.DateModifier/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace DateModifier
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string start = Console.ReadLine();
+            string end = Console.ReadLine();
+
+            Console.WriteLine(DateModifier.GetDifferenceInDays(start, end));
+        }
+    }
+}
