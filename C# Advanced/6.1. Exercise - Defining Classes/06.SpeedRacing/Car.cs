a6e17ece175f22600fe6bdf0a4632a404e58a11d
SPalahanov <slavcho.palahanov@gmail.com>
2023-10-04 23:50:18 +0300

    Upload 06.SpeedRacing

---
+++ C# Advanced/6.1. Exercise - Defining Classes/06.SpeedRacing/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace SpeedRacing
+{
+    internal class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+        //public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer, double travelDistance)
+        //{
+        //    this.Model = model;
+        //    this.FuelAmount = fuelAmount;
+        //    this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+        //    this.TravelledDistance = travelDistance;
+        //}
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public double FuelAmount
+        {
+            get { return fuelAmount; }
+            set { fuelAmount = value; }
+        }
+
+        public double FuelConsumptionPerKilometer
+        {
+            get { return fuelConsumptionPerKilometer; }
+            set { fuelConsumptionPerKilometer = value; }
+        }
+
+        public double TravelledDistance
+        {
+            get { return travelledDistance; }
+            set { travelledDistance = value; }
+        }
+
+        public void CalculateDriving(double distance) 
+        {
+            if (distance * FuelConsumptionPerKilometer > fuelAmount)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+            else
+            {
+                FuelAmount -= distance * FuelConsumptionPerKilometer;
+                TravelledDistance += distance;
+            }
+        }
+    }
+}
---
+++ C# Advanced/6.1. Exercise - Defining Classes/06.SpeedRacing/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace SpeedRacing
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Car> cars = new();
+
+            int carsNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < carsNumber; i++)
+            {
+                string[] carProps = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Car car = new Car()
+                {
+                    Model = carProps[0],
+                    FuelAmount = double.Parse(carProps[1]),
+                    FuelConsumptionPerKilometer = double.Parse(carProps[2]),
+                };
+
+                cars.Add(car.Model, car);
+            }
+
+            string command = String.Empty;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                if (command == "End")
+                {
+                    break;
+                }
+
+                string[] driveProps = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string carModel = driveProps[1];
+                double distance = double.Parse(driveProps[2]);
+
+                Car car = cars[carModel];
+
+                car.CalculateDriving(distance);
+            }
+
+            foreach (var car in cars.Values)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
