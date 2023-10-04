65d56c5f88c7cacc897c963636c19a45522977ba
SPalahanov <slavcho.palahanov@gmail.com>
2023-10-04 23:48:41 +0300

    Upload 03.OldestFamilyMember

---
+++ C# Advanced/6.1. Exercise - Defining Classes/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,34 @@
+﻿namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            people = new List<Person>();
+        }
+
+        public List<Person> People
+        {
+            get
+            {
+                return this.people;
+            }
+            set
+            {
+                this.People = value;
+            }
+        }
+
+        public void AddMember(Person person)
+        {
+            this.People.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.People.MaxBy(p => p.Age);
+        }
+    }
+}
