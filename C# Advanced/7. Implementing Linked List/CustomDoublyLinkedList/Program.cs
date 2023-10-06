80b221d864777289290ca1fd18e106e676509213
SPalahanov <slavcho.palahanov@gmail.com>
2023-10-06 20:10:29 +0300

    Custom Doubly Linked List

---
+++ C# Advanced/7. Implementing Linked List/CustomDoublyLinkedList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Channels;
+
+namespace CustomDoublyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SoftUniLinkedList linkedList = new SoftUniLinkedList();
+            
+            linkedList.AddLast(1);
+            linkedList.AddLast(2);
+            linkedList.AddLast(3);
+            linkedList.AddLast(4);
+
+            linkedList.AddFirst(5);
+            linkedList.AddFirst(5);
+
+
+            linkedList.RemoveFirst();
+
+            linkedList.RemoveLast();
+            
+            
+            int listSum = 0;
+
+            linkedList.ForEach(x =>
+            {
+                listSum += x;
+                Console.WriteLine($"From Foreach: {x}");
+            });
+
+            Console.WriteLine(listSum);
+        }
+    }
+} 
