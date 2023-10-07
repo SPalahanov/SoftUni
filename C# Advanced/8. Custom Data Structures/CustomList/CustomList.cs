78934ac48fba9c427f2063d9cb2a31a10ad1ff80
SPalahanov <slavcho.palahanov@gmail.com>
2023-10-07 18:55:09 +0300

    Custom Data Structures

---
+++ C# Advanced/8. Custom Data Structures/CustomList/CustomList.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    internal class CustomList
+    {
+        private const int InitialCapacity = 2;
+
+        private int[] items;
+
+        public CustomList()
+        {
+            items = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                TrowExceptionIfIndexOutOfRange(index);
+
+                return items[index];
+            }
+            set
+            {
+                TrowExceptionIfIndexOutOfRange(index);
+
+                items[index] = value;
+            }
+        }
+
+        public void Add(int item)
+        {
+            if (items.Length == Count)
+            {
+                Resize();
+            }
+
+            items[Count] = item;
+
+            Count++;
+        }
+
+        public void AddRange(int[] items)
+        {
+            foreach (var item in items)
+            {
+                Add(item);
+            }
+        }
+
+        public int RemoveAt(int index)
+        {
+            TrowExceptionIfIndexOutOfRange(index);
+
+            int removedItem = items[index];
+
+            ShiftLeft(index);
+
+            Count--;
+
+            if (Count == items.Length / 4)
+            {
+                Shrink();
+            }
+
+            return removedItem;
+        }
+
+        public void InsertAt(int index, int item)
+        {
+            TrowExceptionIfIndexOutOfRange(index);
+
+            if (items.Length == Count)
+            {
+                Resize();
+            }
+
+            ShiftRight(index);
+
+            items[index] = item;
+
+            Count++;
+        }
+
+        public bool Contains(int item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (items[i] == item)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            TrowExceptionIfIndexOutOfRange(firstIndex);
+            TrowExceptionIfIndexOutOfRange(secondIndex);
+
+            int temp = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = temp;
+        }
+
+        private void Resize()
+        {
+            int[] copy = new int[items.Length * 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+        }
+
+        private void TrowExceptionIfIndexOutOfRange(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new IndexOutOfRangeException("Invalid index value");
+            }
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            items[Count - 1] = default;
+        }
+
+        private void ShiftRight(int index)
+        {
+            for (int i = Count - 1; i >= index; i--)
+            {
+                items[i + 1] = items[i];
+            }
+        }
+
+        private void Shrink()
+        {
+            int[] copy = new int[items.Length / 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+
+        }
+    }
+}
---
+++ C# Advanced/8. Custom Data Structures/CustomList/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CustomList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList list = new CustomList(); 
+
+            list.Add(5);
+            list.Add(4);
+            list.Add(8);
+            list.Add(1);
+            list.Add(3);
+
+            list[0] = -7;
+            
+            Console.WriteLine(list[2]);
+
+            list.AddRange(new int[] {1, 2, 3, 4});
+
+            Console.WriteLine(list.RemoveAt(1));
+            Console.WriteLine(list.RemoveAt(1));
+            Console.WriteLine(list.RemoveAt(1));
+
+            list.InsertAt(0, -6);
+
+            Console.WriteLine(list.Contains(8));
+            Console.WriteLine(list.Contains(3));
+
+            list.Swap(0, 1);
+
+            Console.WriteLine();
+        }
+    }
+}
