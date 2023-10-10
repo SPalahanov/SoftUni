52d267a6774e6a52d15346fb4ad83f3c490095b8
SPalahanov <slavcho.palahanov@gmail.com>
2023-10-10 19:20:07 +0300

    Generic Array Creator

---
+++ C# Advanced/9. Generics/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int lenght, T elements)
+        {
+            T[] array = new T[lenght];
+
+            for (int i = 0; i < lenght; i++)
+            {
+                array[i] = elements;
+            }
+
+            return array;
+        }
+    }
+}
