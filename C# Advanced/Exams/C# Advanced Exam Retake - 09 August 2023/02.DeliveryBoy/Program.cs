ab12f7c83c4eb1c7d52358e83fe1892353085d6e
SPalahanov <slavcho.palahanov@gmail.com>
2023-10-18 21:36:27 +0300

    C# Advanced Exam Retake - 09 August 2023

---
+++ C# Advanced/Exams/C# Advanced Exam Retake - 09 August 2023/02.DeliveryBoy/Program.cs
@@ -0,0 +1,210 @@
+﻿using System.Data;
+
+namespace _02.DeliveryBoy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            int boyRow = 0;
+            int boyCol = 0;
+
+            int boyStartRow = 0;
+            int boyStartCol = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                string newRow = Console.ReadLine();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = newRow[col];
+
+                    if (matrix[row, col] == 'B')
+                    {
+                        boyRow = row;
+                        boyCol = col;
+
+                        boyStartRow = row;
+                        boyStartCol = col;
+                    }
+                }
+            }
+
+            bool isOutSideMatrix = false;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "left")
+                {
+                    if (boyCol == 0)
+                    {
+                        if (matrix[boyRow, boyCol] == '-')
+                        {
+                            matrix[boyRow, boyCol] = '.';
+                        }
+
+                        isOutSideMatrix = true;
+
+                        Console.WriteLine("The delivery is late. Order is canceled.");
+
+                        break;
+                    }
+                    else
+                    {
+                        if (matrix[boyRow, boyCol - 1] == '*')
+                        {
+                            continue;
+                        }
+
+                        if (matrix[boyRow, boyCol] != 'R')
+                        {
+                            matrix[boyRow, boyCol] = '.';
+                        }
+
+                        boyCol--;
+                    }
+                }
+                else if (command == "right")
+                {
+                    if (boyCol == cols - 1)
+                    {
+                        if (matrix[boyRow, boyCol] == '-')
+                        {
+                            matrix[boyRow, boyCol] = '.';
+                        }
+
+                        isOutSideMatrix = true;
+
+                        Console.WriteLine("The delivery is late. Order is canceled.");
+
+                        break;
+                    }
+                    else
+                    {
+                        if (matrix[boyRow, boyCol + 1] == '*')
+                        {
+                            continue;
+                        }
+
+                        if (matrix[boyRow, boyCol] != 'R')
+                        {
+                            matrix[boyRow, boyCol] = '.';
+                        }
+
+                        boyCol++;
+                    }
+                }
+                else if (command == "up")
+                {
+                    if (boyRow == 0)
+                    {
+                        if (matrix[boyRow, boyCol] == '-')
+                        {
+                            matrix[boyRow, boyCol] = '.';
+                        }
+
+                        isOutSideMatrix = true;
+
+                        Console.WriteLine("The delivery is late. Order is canceled.");
+
+                        break;
+                    }
+                    else
+                    {
+                        if (matrix[boyRow - 1, boyCol] == '*')
+                        {
+                            continue;
+                        }
+
+                        if (matrix[boyRow, boyCol] != 'R')
+                        {
+                            matrix[boyRow, boyCol] = '.';
+                        }
+
+                        boyRow--;
+                    }
+                }
+                else if (command == "down")
+                {
+                    if (boyRow == rows - 1)
+                    {
+                        if (matrix[boyRow, boyCol] == '-')
+                        {
+                            matrix[boyRow, boyCol] = '.';
+                        }
+
+                        isOutSideMatrix = true;
+
+                        Console.WriteLine("The delivery is late. Order is canceled.");
+
+                        break;
+                    }
+                    else
+                    {
+                        if (matrix[boyRow + 1, boyCol] == '*')
+                        {
+                            continue;
+                        }
+
+                        if (matrix[boyRow, boyCol] != 'R')
+                        {
+                            matrix[boyRow, boyCol] = '.';
+                        }
+
+                        boyRow++;
+                    }
+                }
+
+                if (matrix[boyRow, boyCol] == 'P')
+                {
+                    matrix[boyRow, boyCol] = 'R';
+
+                    Console.WriteLine("Pizza is collected. 10 minutes for delivery.");
+
+                    continue;
+                }
+
+                if (matrix[boyRow, boyCol] == 'A')
+                {
+                    matrix[boyRow, boyCol] = 'P';
+
+                    Console.WriteLine("Pizza is delivered on time! Next order...");
+
+                    break;
+                }
+            }
+
+            if (isOutSideMatrix)
+            {
+                matrix[boyStartRow, boyStartCol] = ' ';
+            }
+            else
+            {
+                matrix[boyStartRow, boyStartCol] = 'B';
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(string.Join(" ", matrix[row, col]));
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/Exams/C# Advanced Exam Retake - 09 August 2023/03.VendingSystem/VendingMachine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingSystem
+{
+    public class VendingMachine
+    {
+        public VendingMachine(int buttonCapacity)
+        {
+            Drinks = new List<Drink>();
+            ButtonCapacity = buttonCapacity;
+        }
+
+        public int ButtonCapacity { get; private set; }
+        public List<Drink> Drinks { get; private set; }
+        public int GetCount => Drinks.Count;
+
+        //public int GetCount
+        //{
+        //    get { return Drinks.Count; }
+        //}
+
+
+        public void AddDrink(Drink drink)
+        {
+            if (GetCount < ButtonCapacity)
+            {
+                Drinks.Add(drink);
+            }
+        }
+
+        public bool RemoveDrink(string name) => Drinks.Remove(Drinks.FirstOrDefault(d => d.Name == name));
+
+        public Drink GetLongest() => Drinks.MaxBy(d => d.Volume);
+
+        public Drink GetCheapest() => Drinks.MinBy(d => d.Price);
+
+        public string BuyDrink(string name) => Drinks.FirstOrDefault(d => d.Name == name).ToString();
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Drinks available:");
+
+            foreach (var drink in Drinks)
+            {
+                sb.AppendLine(drink.ToString());
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
