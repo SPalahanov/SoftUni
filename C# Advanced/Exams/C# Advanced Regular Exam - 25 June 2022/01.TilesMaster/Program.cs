cb6ffa1cd1593d7c74dc10a62fa8c92ea4179f6f
SPalahanov <slavcho.palahanov@gmail.com>
2023-10-21 08:39:54 +0300

    C# Advanced Regular Exam - 25 June 2022

---
+++ C# Advanced/Exams/C# Advanced Regular Exam - 25 June 2022/01.TilesMaster/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TilesMaster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> white = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            Queue<int> grey = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            Dictionary<string, int> locations = new Dictionary<string, int>();
+
+            int countertopTiles = 0;
+            int flootTiles = 0;
+            int ovenTiles = 0;
+            int sinkTiles = 0;
+            int wallTiles = 0;
+
+            while (white.Any() && grey.Any())
+            {
+                int whiteTiles = white.Pop();
+                int greyTiles = grey.Dequeue();
+
+                if (whiteTiles == greyTiles)
+                {
+                    if (whiteTiles + greyTiles == 40)
+                    {
+                        if (!locations.ContainsKey("Sink"))
+                        {
+                            locations.Add("Sink", sinkTiles);
+                        }
+
+                        sinkTiles++;
+                        locations["Sink"] = sinkTiles;
+
+                    }
+                    else if (whiteTiles + greyTiles == 50)
+                    {
+                        if (!locations.ContainsKey("Oven"))
+                        {
+                            locations.Add("Oven", ovenTiles);
+                        }
+
+                        ovenTiles++;
+                        locations["Oven"] = ovenTiles;
+                    }
+                    else if (whiteTiles + greyTiles == 60)
+                    {
+                        if (!locations.ContainsKey("Countertop"))
+                        {
+                            locations.Add("Countertop", countertopTiles);
+                        }
+
+                        countertopTiles++;
+                        locations["Countertop"] = countertopTiles;
+                    }
+                    else if (whiteTiles + greyTiles == 70)
+                    {
+                        if (!locations.ContainsKey("Wall"))
+                        {
+                            locations.Add("Wall", wallTiles);
+                        }
+
+                        wallTiles++;
+                        locations["Wall"] = wallTiles;
+                    }
+                    else
+                    {
+                        if (!locations.ContainsKey("Floor"))
+                        {
+                            locations.Add("Floor", flootTiles);
+                        }
+
+                        flootTiles++;
+                        locations["Floor"] = flootTiles;
+                    }
+                }
+                else
+                {
+                    white.Push(whiteTiles / 2);
+                    grey.Enqueue(greyTiles);
+                }
+            }
+
+            if (white.Any())
+            {
+                Console.WriteLine($"White tiles left: {string.Join(", ", white)}");
+            }
+            else
+            {
+                Console.WriteLine("White tiles left: none");
+            }
+
+            if (grey.Any())
+            {
+                Console.WriteLine($"Grey tiles left: {string.Join(", ", grey)}");
+            }
+            else
+            {
+                Console.WriteLine("Grey tiles left: none");
+            }
+
+            foreach (var location in locations.OrderByDescending(l => l.Value).ThenBy(l => l.Key))
+            {
+                Console.WriteLine($"{location.Key}: {location.Value}");
+            }
+        }
+    }
+}
---
+++ C# Advanced/Exams/C# Advanced Regular Exam - 25 June 2022/02.WallDestroyer/Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+
+namespace WallDestroyer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int rows = size;
+            int cols = size;
+
+            char[,] matrix = new char[rows, cols];
+
+            int vankoRow = 0;
+            int vankoCol = 0;
+
+            int countOfHoles = 1;
+            int countOfRods = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                string newRow = Console.ReadLine();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = newRow[col];
+
+                    if (matrix[row, col] == 'V')
+                    {
+                        vankoRow = row;
+                        vankoCol = col;
+
+                        matrix[row, col] = '*';
+                    }
+                }
+            }
+
+            bool isElectrocuted = false;
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                if (command == "left")
+                {
+                    if (vankoCol == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (matrix[vankoRow, vankoCol - 1] == 'R')
+                        {
+                            countOfRods++;
+
+                            Console.WriteLine("Vanko hit a rod!");
+                            continue;
+                        }
+                        else if (matrix[vankoRow, vankoCol - 1] == 'C')
+                        {
+                            matrix[vankoRow, vankoCol - 1] = 'E';
+
+                            isElectrocuted = true;
+
+                            countOfHoles++;
+
+                            break;
+                        }
+                        else if (matrix[vankoRow, vankoCol - 1] == '*')
+                        {
+                            Console.WriteLine($"The wall is already destroyed at position [{vankoRow}, {vankoCol - 1}]!");
+                        }
+                        else if (matrix[vankoRow, vankoCol - 1] == '-')
+                        {
+                            matrix[vankoRow, vankoCol - 1] = '*';
+
+                            countOfHoles++;
+                        }
+
+                        vankoCol--;
+                    }
+                }
+
+                if (command == "right")
+                {
+                    if (vankoCol == cols - 1)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (matrix[vankoRow, vankoCol + 1] == 'R')
+                        {
+                            countOfRods++;
+
+                            Console.WriteLine("Vanko hit a rod!");
+                            continue;
+                        }
+                        else if (matrix[vankoRow, vankoCol + 1] == 'C')
+                        {
+                            matrix[vankoRow, vankoCol + 1] = 'E';
+
+                            isElectrocuted = true;
+
+                            countOfHoles++;
+
+                            break;
+                        }
+                        else if (matrix[vankoRow, vankoCol + 1] == '*')
+                        {
+                            Console.WriteLine($"The wall is already destroyed at position [{vankoRow}, {vankoCol + 1}]!");
+                        }
+                        else if (matrix[vankoRow, vankoCol + 1] == '-')
+                        {
+                            matrix[vankoRow, vankoCol + 1] = '*';
+
+                            countOfHoles++;
+                        }
+
+                        vankoCol++;
+                    }
+                }
+
+                if (command == "up")
+                {
+                    if (vankoRow == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (matrix[vankoRow - 1, vankoCol] == 'R')
+                        {
+                            countOfRods++;
+
+                            Console.WriteLine("Vanko hit a rod!");
+                            continue;
+                        }
+                        else if (matrix[vankoRow - 1, vankoCol] == 'C')
+                        {
+                            matrix[vankoRow - 1, vankoCol] = 'E';
+
+                            isElectrocuted = true;
+
+                            countOfHoles++;
+
+                            break;
+                        }
+                        else if (matrix[vankoRow - 1, vankoCol] == '*')
+                        {
+                            Console.WriteLine($"The wall is already destroyed at position [{vankoRow - 1}, {vankoCol}]!");
+                        }
+                        else if (matrix[vankoRow - 1, vankoCol] == '-')
+                        {
+                            matrix[vankoRow - 1, vankoCol] = '*';
+
+                            countOfHoles++;
+                        }
+
+                        vankoRow--;
+                    }
+                }
+
+                if (command == "down")
+                {
+                    if (vankoRow == rows - 1)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (matrix[vankoRow + 1, vankoCol] == 'R')
+                        {
+                            countOfRods++;
+
+                            Console.WriteLine("Vanko hit a rod!");
+                            continue;
+                        }
+                        else if (matrix[vankoRow + 1, vankoCol] == 'C')
+                        {
+                            matrix[vankoRow + 1, vankoCol] = 'E';
+
+                            isElectrocuted = true;
+
+                            countOfHoles++;
+
+                            break;
+                        }
+                        else if (matrix[vankoRow + 1, vankoCol] == '*')
+                        {
+                            Console.WriteLine($"The wall is already destroyed at position [{vankoRow + 1}, {vankoCol}]!");
+                        }
+                        else if (matrix[vankoRow + 1, vankoCol] == '-')
+                        {
+                            matrix[vankoRow + 1, vankoCol] = '*';
+
+                            countOfHoles++;
+                        }
+
+                        vankoRow++;
+                    }
+                }
+            }
+
+            if (isElectrocuted)
+            {
+                Console.WriteLine($"Vanko got electrocuted, but he managed to make {countOfHoles} hole(s).");
+            }
+            else
+            {
+                Console.WriteLine($"Vanko managed to make {countOfHoles} hole(s) and he hit only {countOfRods} rod(s).");
+
+                matrix[vankoRow, vankoCol] = 'V';
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(string.Join("", matrix[row, col]));
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
